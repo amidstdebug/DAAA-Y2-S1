dac48f0f62d97da99672f68e9c185ad9424fb9ef
Justin Wong JH (amidstdebug@github) <63507306+amidstdebug@users.noreply.github.com>
2022-04-27 17:13:24 +0800

    Started on CA1

--- AIML/test1.js
+++
@@ -1,6 +0,0 @@
-let num = 8;
-console.log((num % 2 > 0) || (num % 3 < 0));
-
-
-
-
--- AIML/test1.py
+++
@@ -1,6 +0,0 @@
-<<<<<<< Updated upstream
-import tensorflow as tf
-tf.config.list_physicael_devices('GPU')
-=======
-print('hello')q
->>>>>>> Stashed changes
